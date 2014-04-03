b9becf9edfa031f59f4358be14eef9e0679efe5f
Victor Wilson <victor.wilson@gmail.com>
2014-04-02 23:58:24 -0500

    added toString()

--- CS499/src/main/java/cs499blue/models/Path.java
+++ CS499/src/main/java/cs499blue/models/Path.java
@@ -1,5 +1,7 @@
 package cs499blue.models;
 
+import cs499blue.algorithms.Distance;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -15,12 +17,34 @@ public class Path implements Iterable<Vertex> {
         thePath = new ArrayList<>();
     }
 
-    public void AddNextVertex(Vertex v) {
+    public void addNextVertex(Vertex v) {
         thePath.add(v);
+    }
+
+    public Double getLength() {
+        Double accumulator = 0d;
+        for (int i = 0; i < thePath.size() - 1; i++) {
+            accumulator += Distance.distanceFrom(thePath.get(i), thePath.get(i + 1));
+        }
+        return accumulator;
     }
 
     @Override
     public Iterator<Vertex> iterator() {
         return thePath.iterator();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder ts = new StringBuilder("Shortest Path:\n");
+
+        for (Vertex v : thePath) {
+            ts.append(v + "\n");
+        }
+
+        ts.append("Final Distance: ");
+        ts.append(getLength());
+
+        return ts.toString();
     }
 }
